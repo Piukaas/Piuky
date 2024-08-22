257f83fe46e2c19dfb15d88075434bf2f7975a8f
Thijs <thijsvanrijsbergen@gmail.com>
2024-08-22 13:26:36 +0200

    style: Update NavBar component with new logo and menu button styles

--- src/components/core/NavBar.vue
+++ src/components/core/NavBar.vue
@@ -1,13 +1,43 @@
 <template>
-  <menu-button />
+  <nav class="navbar">
+    <menu-button />
+    <router-link to="/fortnite" class="logo-container">
+      <img :src="Logo" alt="logo" class="logo" />
+    </router-link>
+  </nav>
 </template>
 
 <script>
+import Logo from "@/assets/logo.png";
 import MenuButton from "@/components/items/MenuButton.vue";
 
 export default {
   components: {
     MenuButton,
   },
+
+  data() {
+    return {
+      Logo,
+    };
+  },
 };
 </script>
+
+<style scoped>
+.navbar {
+  position: fixed;
+  background-color: transparent !important;
+  width: 100%;
+}
+
+.logo-container {
+  flex-grow: 1;
+  display: flex;
+  justify-content: center;
+}
+
+.logo {
+  width: 15%;
+}
+</style>
