12c9bd99eca21038f613f391dc1fa87df6d8811c
Thijs <thijsvanrijsbergen@gmail.com>
2024-08-25 13:44:27 +0200

    style: Adjust layout and margins for responsive design in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <NavBar />
-  <div class="router-view-wrapper">
-    <router-view class="router-view-content"></router-view>
+  <div class="row router-view-wrapper">
+    <router-view class="col-lg-8 col-6 router-view-content"></router-view>
   </div>
 </template>
 
@@ -21,19 +21,11 @@ export default {
   justify-content: center;
   align-items: center;
   min-height: 100vh;
-  max-width: 100vw;
 }
 
-.router-view-content {
-  margin-left: 220px;
-  margin-right: 220px;
-}
-
-@media (max-width: 600px) {
+@media (max-width: 1000px) {
   .router-view-content {
-    margin-top: 150px;
-    margin-left: 75px;
-    margin-right: 75px;
+    margin: 100px 0;
   }
 }
 </style>
